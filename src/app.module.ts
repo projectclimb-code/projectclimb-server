621740171f3a9be562558586b836fc288385b334
lundeee <lundeee@gmail.com>
2024-10-15 09:31:54 +0200

    *

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,16 +2,14 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { MulterModule } from '@nestjs/platform-express';
-import { ConfigModule } from '@nestjs/config';
 import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
   imports: [
-    MulterModule.register({
-      dest: './files',
-    }),
-    ConfigModule.forRoot(),
     MongooseModule.forRoot(process.env.DATABASE_URL),
+    MulterModule.register({
+      dest: process.env.FILE_STORAGE,
+    }),
   ],
   controllers: [AppController],
   providers: [AppService],
--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,11 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
+  console.log('Starting app on port: \t', process.env.PORT);
+  console.log('Connecting to DB: \t', process.env.DATABASE_URL);
+  console.log('Files locatiom: \t', process.env.FILE_STORAGE);
+
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  await app.listen(process.env.PORT);
 }
 bootstrap();
