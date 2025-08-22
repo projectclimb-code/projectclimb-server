713dcbeb3940fe96850735ae67c189388f37b177
maptnh <maptnh@users.noreply.github.com>
2025-08-22 17:20:57 +0200

    separate test websocket endpoint

--- code/climber/consumers.py
+++ code/climber/consumers.py
@@ -38,3 +38,43 @@ class PoseConsumer(AsyncWebsocketConsumer):
 
         # Send message to WebSocket
         await self.send(text_data=message)
+
+
+
+class TestPoseConsumer(AsyncWebsocketConsumer):
+    async def connect(self):
+        self.room_group_name = 'test_pose_stream'
+
+        # Join room group
+        await self.channel_layer.group_add(
+            self.room_group_name,
+            self.channel_name
+        )
+        await self.accept()
+        print("WebSocket connection established.")
+
+    async def disconnect(self, close_code):
+        # Leave room group
+        await self.channel_layer.group_discard(
+            self.room_group_name,
+            self.channel_name
+        )
+        print(f"WebSocket connection closed: {close_code}")
+
+    # Receive message from WebSocket (from the streamer)
+    async def receive(self, text_data):
+        # Broadcast the received data to the web client group
+        await self.channel_layer.group_send(
+            self.room_group_name,
+            {
+                'type': 'pose_message',
+                'message': text_data
+            }
+        )
+
+    # Receive message from room group (to send to the web client)
+    async def pose_message(self, event):
+        message = event['message']
+
+        # Send message to WebSocket
+        await self.send(text_data=message)
--- code/climber/routing.py
+++ code/climber/routing.py
@@ -1,6 +1,7 @@
 from django.urls import re_path
-from .consumers import PoseConsumer
+from .consumers import PoseConsumer, TestPoseConsumer
 
 websocket_urlpatterns = [
     re_path(r'ws/pose/$', PoseConsumer.as_asgi()),
+    re_path(r'ws/test_pose/$', TestPoseConsumer.as_asgi()),
 ]
